7961d7eae6b1dd16b0bf5d789bf04adf441faf33
jamaspy <aspin@me.com>
2019-07-13 14:53:09 +1000

    Updates

---
+++ precache-manifest.d45e14d8d64482812a8e4d6b2c8c82e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f633e9f5c42aee02c8f561c3b664995d",
+    "url": "/jamaspy/palochallenge_client/index.html"
+  },
+  {
+    "revision": "d4d5649fdd7a8590ca58",
+    "url": "/jamaspy/palochallenge_client/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "72c0052f0e897506b1be",
+    "url": "/jamaspy/palochallenge_client/static/js/2.a7d2c5c4.chunk.js"
+  },
+  {
+    "revision": "d4d5649fdd7a8590ca58",
+    "url": "/jamaspy/palochallenge_client/static/js/main.64b5c5e3.chunk.js"
+  },
+  {
+    "revision": "91543c781b272a8a9b4e",
+    "url": "/jamaspy/palochallenge_client/static/js/runtime~main.9a7e628f.js"
+  }
+]);
